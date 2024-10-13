1697f8027725dab94b16d6bc26ea52b3b28a049a
eepodrezov <egor.podrezov@kirarock.team>
2024-10-13 17:04:04 +0300

    Добавил аннотиции к функция bfs dgs

--- algorithms/search/bfs.h
+++ algorithms/search/bfs.h
@@ -8,20 +8,32 @@
 using namespace std;
 using Graph = vector<vector<int>>;
 
-//Обход графа в ширину
+/**
+ * Выполняет обход графа в ширину (BFS).
+ *
+ * @param graph Входной граф, представленный в виде матрицы смежности.
+ * @param startNode Индекс узла, с которого начинается обход.
+ */
 void bfs(const Graph& graph, int startNode) {
+    // Вектор для отслеживания посещенных узлов
     vector<bool> visited(graph.size(), false);
+    // Очередь для хранения узлов для обработки
     queue<int> q;
 
+    // Отмечаем начальный узел как посещенный и добавляем его в очередь
     visited[startNode] = true;
     q.push(startNode);
 
+    // Пока очередь не пуста
     while (!q.empty()) {
+        // Извлекаем узел из очереди
         int node = q.front();
         q.pop();
-        cout << node << " ";
+        cout << node << " ";  // Выводим узел
 
+        // Обрабатываем соседей текущего узла
         for (int neighbor : graph[node]) {
+            // Если сосед еще не был посещен, добавляем его в очередь
             if (!visited[neighbor]) {
                 visited[neighbor] = true;
                 q.push(neighbor);
--- algorithms/search/dfs.h
+++ algorithms/search/dfs.h
@@ -7,11 +7,19 @@
 using namespace std;
 using Graph = vector<vector<int>>;
 
-// Обход графа в глубину
+/**
+ * Выполняет обход графа в глубину (DFS).
+ *
+ * @param graph Входной граф, представленный в виде матрицы смежности.
+ * @param node Индекс узла, с которого начинается обход.
+ * @param visited Вектор для отслеживания посещенных узлов.
+ */
 void dfs(const Graph& graph, int node, vector<bool>& visited) {
+    // Отмечаем узел как посещенный
     visited[node] = true;
-    cout << node << " ";
+    cout << node << " ";  // Выводим узел
 
+    // Рекурсивно обрабатываем всех соседей текущего узла
     for (int neighbor : graph[node]) {
         if (!visited[neighbor]) {
             dfs(graph, neighbor, visited);
